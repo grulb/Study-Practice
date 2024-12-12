1c5877f94be2cb8b0f39df2e7a6ca655441f2e61
GreeiLLL <ilya21070401@gmail.com>
2024-12-12 13:17:30 +0300

    Project created, added authorizarion window

---
+++ LibraryMobileApp/app/src/main/java/com/example/librarymobileapp/RegisterActivity.kt
@@ -0,0 +1,30 @@
+package com.example.librarymobileapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.librarymobileapp.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        goEnter()
+    }
+
+    private fun goEnter() {
+        binding.registerEnterButton.setOnClickListener {
+            finish()
+        }
+    }
+}
